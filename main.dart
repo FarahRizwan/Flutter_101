834e60e838dc115bdb7c460adffb571db8c450f9
FarahRizwan <119890115+FarahRizwan@users.noreply.github.com>
2022-12-06 14:06:35 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+
+void main() {
+  //1) Write a program that input two numbers and if second number is square of first using if
+//statement.
+  int num = int.parse(stdin.readLineSync()!);
+  int secondnum = int.parse(stdin.readLineSync()!);
+
+  int product = num * secondnum;
+  print(product);
+  if (10 * 10 == 100) {
+    print("Condition is true");
+  } else {
+    print("Condition is false");
+  }
+
+//Write a program that input marks of three subjects. If the average of marks is more than
+//80, it display messages “you are above standard” otherwise it display “You are low
+//standard.
+  stdout.write("write eng marks:");
+  stdout.write("enter math marks:");
+  stdout.write("enter urdu marks:");
+  double englishmarks = double.parse(stdin.readLineSync()!);
+  double mathmarks = double.parse(stdin.readLineSync()!);
+  double urdumarks = double.parse(stdin.readLineSync()!);
+  double averagemarks = englishmarks + mathmarks + urdumarks;
+  print(averagemarks / 3);
+  int average = 80;
+  if (average > 80) {
+    print("you are below average");
+  } else {
+    print("you are above average");
+  }
+  //Write a program that input marks of student and display grade according to following
+//criteria:
+  //int marks = 90;
+  //if (marks == 55) {
+  // print("Grade F");
+  //} else if (marks == 75) {
+  //  print("Grade C");
+  // } else if (marks == 89) {
+  //   print("Grade B");
+  // } else if (marks == 90) {
+  //   print("Grade A");
+  // }
+  //4) Write a program that input number of week’s day and display the name of the day. For Example
+//if user enters 1, it display “Monday” and so on using Switch Statement.
+  //stdout.write("enter the number");
+  //int num = int.parse(stdin.readLineSync()!);
+  //switch (num) {
+  //case 1:
+  //print("Monday");
+  //break;
+  //case 2:
+  //print("Tuesday");
+  //break;
+  //case 3:
+  //print("Wednesday");
+  //break;
+  //case 4:
+  //print("Thursday");
+  //break;
+  //case 5:
+  //print("Friday");
+  //break;
+  //case 6:
+  //print("Saturday");
+  //break;
+  //case 7:
+  //print("Sunday");
+  //break;
+  //default:
+  //print("false");
+  //}
+}
