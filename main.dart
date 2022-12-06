b384d37c67dfa9d776e15181288ba5173908e207
FarahRizwan <119890115+FarahRizwan@users.noreply.github.com>
2022-12-06 14:00:09 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,46 @@
+void main() {
+  //Qno.1 write a program in which make rectangle using print statement(*)?
+  //Example#1
+  print("******");
+  print("*    *");
+  print("*    *");
+  print("*    *");
+  print("******");
+
+//Qno.2 write a program to perform all relational operators(<,>,>=,<=).
+//Example#1
+  int no1 = 35;
+  int no2 = 65;
+  bool result = no1 > no2;
+  print(result);
+  int no3 = 37;
+  int no4 = 45;
+  bool result1 = no3 < no4;
+  print(result1);
+  int no5 = 55;
+  int no6 = 70;
+  bool result2 = no5 <= no6;
+  print(result2);
+  int no7 = 80;
+  int no8 = 90;
+  bool result3 = no7 >= no8;
+  print(result3);
+  //Qno.3 Write a program to subtract two constant numbers?
+  const int b = 45, a = 35;
+
+  print(b - a);
+  //Qno.4 Write a program to calculate area of Circle.
+  //Formula: Area of a Circle= 3.14*radius*radius
+  double pi = 3.14;
+  int radius = 576;
+  //radius=24*24= 576
+  print(pi * radius);
+  //Qno.5 write a program to calculate Area of Square.
+  //formula=Area of a Square=side*side
+  //Example 1
+  int side1 = 6, side2 = 6;
+  print(side1 * side2);
+  //Example 2
+  double side3 = 3.5, side4 = 3.5;
+  print(side3 * side4);
+}
